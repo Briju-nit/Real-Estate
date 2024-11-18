5856c25e8bdaaab274f9385b54f97c3086783aab
Aravindhan-a-p <11a02cnn@gmail.com>
2024-11-18 07:19:25 +0000

    changed something

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,32 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+
+import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
+
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+
+  authDomain: "groceries-app-b740c.firebaseapp.com",
+
+  databaseURL: "https://groceries-app-b740c-default-rtdb.firebaseio.com",
+
+  projectId: "groceries-app-b740c",
+
+  storageBucket: "groceries-app-b740c.appspot.com",
+
+  messagingSenderId: "172407271569",
+
+  appId: "1:172407271569:web:79129e281619ba7fd50f30"
+
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
