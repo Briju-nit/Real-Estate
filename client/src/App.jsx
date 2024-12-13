aea128832fceebd6a167aa202144ce577e3a4561
BRIJU  ANAND  D <brijuanand2004@gmail.com>
2024-12-13 17:24:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
-import Footer from './components/Footer';
+import Footer from './components/Footer.jsx';
 
 export default function App() {
   return (
